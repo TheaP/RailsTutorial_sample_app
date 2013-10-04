6c2fabc6e68b93a5de6e7bd0eab17dfb80dcd854
Thea Pepperl <aapepperl@vcu.edu>
2013-10-04 11:31:02 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+	
+	def show
+		@user = User.find(params[:id])
+	end
+
   def new
+	  @user = User.new
   end
+
+  def create
+	  @user = User.new(user_params)	# Note the use of user_params, not params[:user]
+	  if @user.save
+		  flash[:success] = "Welcome to Neonate Connect!"
+		  redirect_to @user
+	  else
+		  render 'new'
+	  end
+  end
+
+  # Using strong paramters in the create action.  (See Listing 7.22)
+  private
+
+  def user_params
+	  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name      "Thea Pepperl"
+		email     "thea@example.com"
+		password  "foobar"
+		password_confirmation "foobar"
+	end
+end
